e07295e0bc263682017f041b95fa6cf8f3546ea9
Weverson Oliveira <timatrixdev@gmail.com>
2026-05-14 20:37:51 -0300

    aula_ok

---
+++ consulta.py
@@ -0,0 +1,3 @@
+from consulta import id_consulta
+assert id_consulta (marcelo)==101
+print("Teste executado com sucesso")
---
+++ marticula.py
@@ -0,0 +1,7 @@
+def id_consulta(marcelo):
+    if(marcelo == 101):
+        print("Cadastrado")
+    else:
+        print("Erro na consulta")
+    
+id_consulta(101)
